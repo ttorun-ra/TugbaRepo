adf0dca70e290fb2e0b73c2e785765bbdacc2a11
yvzydn <yavuzaydinus@gmail.com>
2019-10-25 10:15:34 -0400

    homework is done

---
+++ TugbaProject/src/Day16/ArrayHomework.java
@@ -0,0 +1,24 @@
+package Day16;
+
+public class ArrayHomework {
+
+	public static void main(String[] args) {
+		
+		int myArray[]= {34,5,6,2,3,78,1};
+		
+		double sum=0;
+		double average=0;
+		for(int i=0;i<myArray.length;i++){
+			
+			
+			sum= sum+myArray[i];
+			average=sum/myArray.length;
+		}
+		System.out.println(sum);
+		System.out.println(average);
+
+			
+
+	}
+
+}
