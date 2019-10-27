d517da2c27b547ab073d5f5a2c89cbd7e202ea80
yvzydn <yavuzaydinus@gmail.com>
2019-10-27 10:49:29 -0400

    homework is here

---
+++ TugbaProject/src/Day15/HwOctober26thTask.java
@@ -0,0 +1,19 @@
+package Day15;
+
+public class HwOctober26thTask {
+
+	public static void main(String[] args) {
+		int[] num1 = {3, 2,5};
+        int[] num2 = {3,2, 5};
+        int length1=num1.length;
+        int length2=num2.length;
+
+        for (int i=0;i<length1&& i<length2;i++)
+        if(num1[i]==num2[i]) {
+        }
+        System.out.println("they are same arrays");
+
+
+	}
+
+}
