7d213b1a19c1a5d983b1dce5f2e06ea461e12b9d
yvzydn <yavuzaydinus@gmail.com>
2019-10-27 22:31:29 -0400

    it is completed

--- TugbaProject/src/Homework2/HwOctober27thDimensionalArrays.java
+++ TugbaProject/src/Homework2/HwOctober27thDimensionalArrays.java
@@ -1,4 +1,4 @@
-package Homework;
+package Homework2;
 
 public class HwOctober27thDimensionalArrays {
 
--- TugbaProject/src/Homework2/MultiDimensionalArraysTask1.java
+++ TugbaProject/src/Homework2/MultiDimensionalArraysTask1.java
@@ -1,4 +1,4 @@
-package Day16;
+package Homework2;
 
 public class MultiDimensionalArraysTask1 {
 	public static void main(String[] args) {
@@ -13,18 +13,21 @@ public class MultiDimensionalArraysTask1 {
 //	System.out.println ("the length is" + NL.length);
 //		System.out.println(NL[0][0]+NL[1][0]); 
 		
-		for (int i = 0; i <=NL.length; i++){
+		for (int i = 0; i <NL.length-1; i++){
 			
-		for(int j=0;j<=NL[i].length;j++) {
+		for(int j=0;j<NL[i].length;j++) {
 		
-				System.out.println("NL  " + NL[i][j]);
-				
 				
 //			System.out.println(NL[0][i]+NL[1][i]);
-		}
+		
 
-        
-		}		 
+		System.out.print("\nNL:  " + NL[i][j] + " " + NL[i+1][j]);
+		
+		}		
+		
+		
+		
+	}
 	}
 }
 	
