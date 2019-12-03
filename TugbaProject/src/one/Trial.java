49e56b72870217ab80db19edefa300dd57175f89
yvzydn <yavuzaydinus@gmail.com>
2019-12-03 11:24:30 -0500

    trial class

---
+++ TugbaProject/src/one/Trial.java
@@ -0,0 +1,20 @@
+package one;
+
+public class Trial {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String name="Tugba";
+		StringBuilder b= new StringBuilder();
+		
+		b.append("Aydin");
+		
+		String ne=b.toString();
+		
+		System.out.println(ne);
+
+	}
+	
+
+}
